036ef7cc35b2e7c00cbd7d70905d6726c0b5964d
Nayrd <mr.nayrd@mail.ru>
2024-05-19 16:34:52 +0300

    Updated cogs.mute command

--- cogs/mute.py
+++ cogs/mute.py
@@ -1,7 +1,7 @@
-import datetime
-
 import disnake
 from disnake.ext import commands
+
+import datetime
 
 
 class Mute(commands.Cog):
@@ -10,45 +10,76 @@ class Mute(commands.Cog):
 
     @commands.slash_command(name='мьют', description='Выдаёт мьют участнику сервера')
     @commands.has_permissions(moderate_members=True)
-    async def mute(self, interaction, user: disnake.User, time: str, reason: str):
-        time = datetime.datetime.now() + datetime.timedelta(minutes=int(time))
-        cool_time = disnake.utils.format_dt(time, style='R')
+    async def mute(
+            self,
+            interaction,
+            user: disnake.Member,
+            term: str = commands.Param(name='срок', description="Введите время мьюта"),
+            time: str = commands.Param(name='время', choices=['Дни', 'Минуты', 'Секунды']),
+            reason: str = commands.Param(name='причина', description="Введите причину мьюта")
+    ):
+        if time.lower() == 'дни':
+            term = datetime.datetime.now() + datetime.timedelta(days=int(term))
+        elif time.lower() == 'минуты':
+            term = datetime.datetime.now() + datetime.timedelta(minutes=int(term))
+        elif time.lower() == 'секунды':
+            term = datetime.datetime.now() + datetime.timedelta(seconds=int(term))
+        else:
+            term = datetime.datetime.now() + datetime.timedelta(hours=int(term))
+
+        cool_time = disnake.utils.format_dt(term, style='R')
         bot_reason = f'Модератор: {interaction.author.name}. Причина: {reason}'
 
-        embed_notf = disnake.Embed(
-            title='Модератор использовал комманду "/mute"',
-            description=f'Участник <@{user.id}> был замьючен.\nПо причине: ****{reason}****\nМодератор: <@{interaction.author.id}>\nРазмьют: ****{cool_time}****.',
-            color=disnake.Color.yellow()
-        )
+        if not user.current_timeout:
+            embed_notf = disnake.Embed(
+                title='Модератор использовал комманду "/mute"',
+                description=f'Участник <@{user.id}> был замьючен.\nПо причине: ****{reason}****\nМодератор: <@{interaction.author.id}>\nРазмьют: ****{cool_time}****.',
+                color=disnake.Color.yellow()
+            )
 
-        embed_user = disnake.Embed(
-            title=f'Вы были замьючены на сервере {interaction.guild.name}',
-            description=f'По причине: ****{reason}****\nМодератор: <@{interaction.author.id}>\nРазмьют: ****{cool_time}****.',
-            color=disnake.Color.yellow()
-        )
+            embed_user = disnake.Embed(
+                title=f'Вы были замьючены на сервере {interaction.guild.name}',
+                description=f'По причине: ****{reason}****\nМодератор: <@{interaction.author.id}>\nРазмьют: ****{cool_time}****.',
+                color=disnake.Color.yellow()
+            )
 
-        await interaction.response.send_message(embed=embed_notf)
-        await user.send(embed=embed_user)
-        await interaction.guild.timeout(user, until=time, reason=bot_reason)
+            await interaction.response.send_message(embed=embed_notf)
+            await user.send(embed=embed_user)
+            await interaction.guild.timeout(user, until=term, reason=bot_reason)
+        else:
+            embed_notf = disnake.Embed(
+                title='',
+                description=f'Участник <@{user.id}> уже находиться в мьюте.',
+                color=disnake.Color.yellow()
+            )
+            await interaction.response.send_message(embed=embed_notf, ephemeral=True)
 
     @commands.slash_command(name='размьют', description='Размьючивает участника сервера')
     @commands.has_permissions(moderate_members=True)
-    async def unmute(self, interaction, user: disnake.User):
-        embed_notf = disnake.Embed(
-            title='Модератор использовал комманду "/unmute"',
-            description=f'Участник <@{user.id}> был разамьючен\nМодератор: <@{interaction.author.id}>.',
-            color=disnake.Color.green()
-        )
+    async def unmute(self, interaction, user: disnake.Member):
+        if user.current_timeout:
+            embed_notf = disnake.Embed(
+                title='Модератор использовал комманду "/unmute"',
+                description=f'Участник <@{user.id}> был разамьючен\nМодератор: <@{interaction.author.id}>.',
+                color=disnake.Color.green()
+            )
 
-        embed_user = disnake.Embed(
-            title=f'Вы были разамьючены на сервере {interaction.guild.name}',
-            description=f'Модератор: <@{interaction.author.id}>.',
-            color=disnake.Color.green()
-        )
+            embed_user = disnake.Embed(
+                title=f'Вы были разамьючены на сервере {interaction.guild.name}',
+                description=f'Модератор: <@{interaction.author.id}>.',
+                color=disnake.Color.green()
+            )
 
-        await interaction.guild.timeout(user, until=None, reason=None)
-        await interaction.response.send_message(embed=embed_notf)
-        await user.send(embed=embed_user)
+            await interaction.guild.timeout(user, until=None, reason=None)
+            await interaction.response.send_message(embed=embed_notf)
+            await user.send(embed=embed_user)
+        else:
+            embed_notf = disnake.Embed(
+                title='',
+                description=f'Участник <@{user.id}> не замьючен.',
+                color=disnake.Color.green()
+            )
+            await interaction.response.send_message(embed=embed_notf, ephemeral=True)
 
 
 def setup(bot):
