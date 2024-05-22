3a0b2e3d5e204958d2ef17951430782f0eec9951
Nayrd <mr.nayrd@mail.ru>
2024-05-22 18:44:52 +0300

    Added new cogs.avatar command

---
+++ cogs/avatar.py
@@ -0,0 +1,23 @@
+import disnake
+from disnake.ext import commands
+
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name='аватар', description='Даёт возмонжость найти и скачать понравившийся вам аватар любого участника сервера')
+    async def avatar(self, interaction, member: disnake.Member = None):
+        user = member or interaction.author
+
+        embed = disnake.Embed(
+            title=f'Аватар пользователя ****{user.display_name}****',
+            color=disnake.Color.light_gray()
+        )
+        embed.set_image(url=user.display_avatar.url)
+
+        await interaction.response.send_message(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
